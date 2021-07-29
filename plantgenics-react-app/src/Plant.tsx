cd29c696e66aca28ffe5a5704efae08dd6bdb324
Abigail Zhong <az383@cornell.edu>
2021-07-28 21:57:55 -0400

    pop up working

--- plantgenics-react-app/src/Plant.tsx
+++ plantgenics-react-app/src/Plant.tsx
@@ -1,6 +1,6 @@
 class Plant{
     constructor(public id: number, public name: string, public type: string, 
-        public image: string, public price: number){
+        public image: string, public price: number, public description?: string){
             this.id = id;
             this.name = name;
             this.type = type;
--- plantgenics-react-app/src/displayPlants.tsx
+++ plantgenics-react-app/src/displayPlants.tsx
@@ -2,6 +2,7 @@ import React, { MouseEventHandler, ReactElement } from 'react';
 import Plant from './Plant';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSeedling } from '@fortawesome/free-solid-svg-icons';
+import * as CSS from 'csstype';
 
 type Props = {plants: Plant[], addPlantToWishlist: (plant: Plant) => void; };
 
@@ -18,16 +19,34 @@ function DisplayPlants({plants, addPlantToWishlist}:Props):ReactElement<any, any
                 </div>
 
                 <div className="card-body">
-                    <h5 className="card-title">{plant.name} Seeds</h5>
+                    <h5 className="card-title" data-toggle="modal" data-target={`#${plant.name}-modal`}>{plant.name} Seeds</h5>
                     <p className="card-text">
                         <div className="bold">Price per ounce: ${plant.price}</div> 
                     </p>
                     <p className="card-text">
                         <div className="bold">Type: {plant.type}</div>
                     </p>
+                </div>
 
+                <div className="modal fade" id={`${plant.name}-modal`} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">{plant.name}</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        <p>{plant.description}</p>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-         </div>
             
           ) }
       </div>
